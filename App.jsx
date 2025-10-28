7c3585cd26fd5911d52815d80789cc0475724dd9
Mrkm6062 <akm222143@gmail.com>
2025-10-28 21:57:08 +0530

    cart function update

--- App.jsx
+++ App.jsx
@@ -253,6 +253,7 @@ function App() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
+          'Authorization': `Bearer ${token}`,
         },
         body: JSON.stringify({ cart: cartData })
       });
