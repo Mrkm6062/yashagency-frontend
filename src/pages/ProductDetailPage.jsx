b505a451980522c68a23ba483f44e29718e58526
Mrkm6062 <akm222143@gmail.com>
2025-12-02 23:46:35 +0530

    imges section is changed 3.0

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -548,7 +548,7 @@ function ProductDetailPage({ products, addToCart, wishlistItems, fetchWishlist, 
             )}
           </div>
         </div>
-        <div className="mt-6">
+        <div className="">
           <SuggestedProducts allProducts={products} currentProductId={product._id} currentCategory={product.category} />
         </div>
 
