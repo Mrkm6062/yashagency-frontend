002cfd58cc1bc16306eb18a6885fc25ed90f7689
Mrkm6062 <akm222143@gmail.com>
2025-12-04 01:12:41 +0530

    seo 100

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = React.memo(function Footer({ API_BASE, LOGO_URL }) {
           <div className="pr-4">
             <picture>
               <source srcSet={getOptimizedImageUrl(LOGO_URL, { format: 'webp', width: 400 })} type="image/webp" />
-              <img src={LOGO_URL} alt="SamriddhiShop" className="h-20 w-auto mb-4" />
+              <img src={LOGO_URL} alt="SamriddhiShop" width="227" height="67" className="w-auto mb-4" />
             </picture>
             <p className="text-gray-300 text-sm">Your trusted online shopping destination for quality products at great prices.</p>
           </div>
