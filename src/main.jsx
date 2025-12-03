97ec70c63e88db31f0746317fe0629b73616eb05
Mrkm6062 <akm222143@gmail.com>
2025-12-03 20:11:14 +0530

    home page seo updated 3.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 // c:\Users\PC\Desktop\Amazon Q CLI\SamriddhiShop\frontend\src\main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App.jsx'; // This should now correctly find App.jsx in the same folder
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 );
 
